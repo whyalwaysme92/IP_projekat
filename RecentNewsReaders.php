79be510d9d58684c9142b65f5251a0b8a1af8ec3
Ognjen Stamenkovic <ostamenkovic@proton.me>
2025-07-02 23:24:34 +0200

    RecentNews za sve korisnike da vuce iz baze

--- RecentNewsReaders.php
+++ RecentNewsReaders.php
@@ -1,105 +1,82 @@
+<?php
+$connection = new mysqli("localhost", "root", "", "bazag01");
+if ($connection->connect_error) {
+    die("Greška prilikom povezivanja sa bazom: " . $connection->connect_error);
+}
+
+$sql = "SELECT IDVesti, Naslov, Apstrakt, Datum, Ocena, PrvaSlikaVesti 
+        FROM vesti 
+        ORDER BY Datum DESC";
+
+$result = $connection->query($sql);
+?>
+
 <!DOCTYPE html> 
 <html>
-    <head>
-        <title>Najnovije vesti - newS</title>
-        <meta charset="UTF-8">
-        <link rel="stylesheet" type="text/css" href="css/style_News.css"> 
-    </head>
-    <body>
-        <div class="PageContentDiv">
-            <div class="PageContent">
-                <div class="Header">
-                    <div class="HeaderLogoImage">
-                        <p>new<span>S</span></p>
-                    </div>
-                    <div class="HeaderNavigation">
-                        <ul>
-                            <li><a href="ReadersMainPage.php">Naslovna</a></li>
-                            <li>
-                                <a href="">Vesti</a>
-                                <ul>
-                                    <li><a href="RecentNewsReaders.php">Najnovije</a></li>
-                                    <li><a href="PopularNewsReaders.php">Najpopularnije</a></li>
-                                    <li><a href="NewsFromLikedAuthorsReaders.php">Omiljeni&nbsp;autori</a></li>
-                                    <li><a href="NewsSoulmatesReaders.php">Srodne&nbsp;duše</a></li>
-                                </ul>
-                            </li>
-                            <li><a href="LikedAuthorsReaders.php">Omiljeni&nbsp;autori</a></li>
-                            <li><a href="Soulmates.php">Srodne&nbsp;duše</a></li>
-                            <li><a href="">Izloguj&nbsp;se</a></li>
-                        </ul>
-                    </div>
+<head>
+    <title>Najnovije vesti - newS</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="css/style_News.css"> 
+</head>
+<body>
+    <div class="PageContentDiv">
+        <div class="PageContent">
+            <div class="Header">
+                <div class="HeaderLogoImage">
+                    <p>new<span>S</span></p>
                 </div>
-        
-                <div class="News">
-                    <div class="HeadingDiv">
-                        <h1>Najnovije vesti</h1>
-                    </div>
-                    <div class="AllArticles">
-                        <div class="OneArticle">
-                            <div class="ArticleInformations">
-                                <div class="ArticleHeadingDiv">
-                                    <a href="">Slovačka zatvara Kancelariju za vezu u Prištini</a>
-                                </div>
-                                <div class="ArticleDateDiv">
-                                    <p>Ocena: </p><p>4.8</p><p> | </p>
-                                </div>
-                                <div class="ArticleDateDiv">
-                                    <p>11-11-2024</p>
-                                </div>
-                                <div class="ArticleAbstractDiv">
-                                    <p>Slovačka, jedna od pet zemalja Evropske unije koje ne priznaju
-                                        jednostrano proglašenu nezavisnost tzv. Kosova, odlučila je da zatvori...
-                                    </p>
-                                </div>
-                            </div><div class="ArticleImageDiv">
-                                <img src="images/slovacka.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="OneArticle">
-                            <div class="ArticleInformations">
-                                <div class="ArticleHeadingDiv">
-                                    <a href="">Zec Fića izgubio šargarepu u Tašmajdanskom parku</a>
-                                </div>
-                                <div class="ArticleDateDiv">
-                                    <p>Ocena: </p><p>4.7</p><p> | </p>
-                                </div>
-                                <div class="ArticleDateDiv">
-                                    <p>11-11-2024</p>
-                                </div>
-                                <div class="ArticleAbstractDiv">
-                                    <p>Nakon obilnih padavina u našem glavnom gradu, zec Fića nije 
-                                        uspeo da pronađe svoju šargarepu koju je ostavio na obližnjoj klupi...
-                                    </p>
-                                </div>
-                            </div><div class="ArticleImageDiv">
-                                <img src="images/zeka.jpg" alt="">
-                            </div>
-                        </div>
-                        <div class="OneArticle">
-                            <div class="ArticleInformations">
-                                <div class="ArticleHeadingDiv">
-                                    <a href="">Sferopulos pred Monako: Ne možemo se fokusirati na jednog igrača</a>
-                                </div>
-                                <div class="ArticleDateDiv">
-                                    <p>Ocena: </p><p>4.6</p><p> | </p>
-                                </div>
-                                <div class="ArticleDateDiv">
-                                    <p>10-11-2024</p>
-                                </div>
-                                <div class="ArticleAbstractDiv">
-                                    <p>Trener košarkaša Crvene zvezde Janis Sveropulos kaže da je Monako
-                                        veoma dobar tim, koji je napravljen da ode na fajnal-for Evrolige..
-                                    </p>
-                                </div>
-                            </div><div class="ArticleImageDiv">
-                                <img src="images/sferopulos.jpg" alt="">
-                            </div>
-                        </div>
-                    </div>
+                <div class="HeaderNavigation">
+                    <ul>
+                        <li><a href="ReadersMainPage.php">Naslovna</a></li>
+                        <li>
+                            <a href="">Vesti</a>
+                            <ul>
+                                <li><a href="RecentNewsReaders.php">Najnovije</a></li>
+                                <li><a href="PopularNewsReaders.php">Najpopularnije</a></li>
+                                <li><a href="NewsFromLikedAuthorsReaders.php">Omiljeni&nbsp;autori</a></li>
+                                <li><a href="NewsSoulmatesReaders.php">Srodne&nbsp;duše</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="LikedAuthorsReaders.php">Omiljeni&nbsp;autori</a></li>
+                        <li><a href="Soulmates.php">Srodne&nbsp;duše</a></li>
+                        <li><a href="Logout.php">Izloguj&nbsp;se</a></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="News">
+                <div class="HeadingDiv">
+                    <h1>Najnovije vesti</h1>
+                </div>
+                <div class="AllArticles">
+                    <?php
+                    if ($result->num_rows > 0) {
+                        while ($row = $result->fetch_assoc()) {
+                            $date = new DateTime($row['Datum']);
+                            echo '<div class="OneArticle">';
+                            echo '<div class="ArticleInformations">';
+                            echo '<div class="ArticleHeadingDiv"><a href="Author.php?id=' . htmlspecialchars($row['IDVesti']) . '">' . htmlspecialchars($row['Naslov']) . '</a></div>';
+                            echo '<div class="ArticleDateDiv">';
+                            echo '<p>Ocena: </p><p>' . htmlspecialchars($row['Ocena']) . '</p><p> | </p>';
+                            echo '<p>' . $date->format('d-m-Y') . '</p>';
+                            echo '</div>';
+                            echo '<div class="ArticleAbstractDiv"><p>' . htmlspecialchars($row['Apstrakt']) . '</p></div>';
+                            echo '</div>';
+                            echo '<div class="ArticleImageDiv">';
+                            if (!empty($row['PrvaSlikaVesti'])) {
+                                echo '<img src="' . htmlspecialchars($row['PrvaSlikaVesti']) . '" alt="Slika vesti">';
+                            }
+                            echo '</div>';
+                            echo '</div>';
+                        }
+                    } else {
+                        echo "<p>Nema dostupnih vesti.</p>";
+                    }
+                    $connection->close();
+                    ?>
                 </div>
             </div>
         </div>
-
-    </body>
+    </div>
+</body>
 </html>
